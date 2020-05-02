c9815bf73dedf926c337371366946af30f50844f
bannzai <yudai-hirose@cookpad.com>
2020-05-03 00:16:19 +0900

    :sparkles: add GedatsuExample

---
+++ GedatsuExample/GedatsuExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GedatsuExample
+//
+//  Created by yudai-hirose on 2020/05/03.
+//  Copyright © 2020 bannzai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
