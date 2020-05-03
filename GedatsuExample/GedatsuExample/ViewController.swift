ef4601c9bb4f65c3896c62c0fb5c320d76c4ea00
bannzai <kingkong999yhirose@gmail.com>
2020-05-04 00:16:59 +0900

    :sparkles: add test data

--- GedatsuExample/GedatsuExample/ViewController.swift
+++ GedatsuExample/GedatsuExample/ViewController.swift
@@ -8,20 +8,56 @@
 
 import UIKit
 
+class CustomView: UIView { }
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var customView: CustomView!
     override func viewDidLoad() {
         super.viewDidLoad()
+
+//        let subview = UIView()
+//        subview.translatesAutoresizingMaskIntoConstraints = false
+//        NSLayoutConstraint.activate([
+//            subview.widthAnchor.constraint(equalToConstant: 100),
+//            subview.widthAnchor.constraint(equalToConstant: 10),
+//        ])
+//        subview.setNeedsLayout()
+//        subview.layoutIfNeeded()
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
         
-        let subview = UIView()
-        NSLayoutConstraint.activate([
-            subview.widthAnchor.constraint(equalToConstant: 100),
-            subview.widthAnchor.constraint(equalToConstant: 10),
-        ])
-        subview.translatesAutoresizingMaskIntoConstraints = false
-        subview.layoutIfNeeded()
-        
-        print("aaaaa")
+        printConstraint()
+    }
+    
+    func printConstraint() {
+        print("view: ----")
+        func _print(constraint: NSLayoutConstraint) {
+            assert(constraint.firstItem != nil)
+            print("  ", "constant: ", constraint.constant)
+            print("  ", "firstItem: ", String(describing: constraint.firstItem))
+            print("  ", "firstItem.frame: ", String(describing: constraint.firstItem?.frame))
+            print("  ", "firstAttribute: ", constraint.firstAttribute.rawValue)
+            print("  ", "secondItem: ", String(describing: constraint.secondItem))
+            print("  ", "secondItem.frame: ", String(describing: constraint.secondItem?.frame))
+            print("  ", "secondAttribute: ", constraint.secondAttribute.rawValue)
+        }
+        view.constraints.enumerated().forEach {
+            print("\($0.0) --- ")
+            _print(constraint: $0.1)
+        }
+        print("customView: ----")
+        customView.constraints.enumerated().forEach {
+            print("\($0): ---")
+            _print(constraint: $1)
+        }
+        print("stackView: ----")
+        stackView.constraints.enumerated().forEach {
+            print("\($0.0) --- ")
+            _print(constraint: $0.1)
+        }
     }
 }
 
