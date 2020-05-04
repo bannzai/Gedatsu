fdb77c3ab2a8f9eb726e680e68d8b19c82c880cd
bannzai <kingkong999yhirose@gmail.com>
2020-05-04 22:51:21 +0900

    :recycle: fix test data
    #
    # ==== Emojis ====
    # 🐛  :bug: バグ修正
    # 🤘  :metal: 機能改善
    # 🐚  :shell: コマンドを実行した結果とか
    # ✨  :sparkles: 部分的な機能追加
    # 🎉  :tada: 盛大に祝うべき大きな機能追加
    # 💉  :syringe: libraryの追加
    # 📝  :memo: ドキュメント・READMEの整備
    # ♻️   :recycle: リファクタリング
    # 🚿  :shower: 不要な機能・使われなくなった機能の削除
    # 💚  :green_heart: テストやCIの修正・改善
    # 👕  :shirt: Lintエラーの修正やコードスタイルの修正
    # 🚀  :rocket: パフォーマンス改善
    # 🆙  :up: 依存パッケージなどのアップデート
    # 🔒  :lock: 新機能の公開範囲の制限
    # 👮  :cop: セキュリティ関連の改善
    # 🎨  :art: デザイン修正
    # 🚧  :construction: WIP 一時的なコミット
    
    # ==== Format ====
    # :emoji: Subject
    #
    # Commit body...
    
    # ==== The Seven Rules ====
    # 1. Separate subject from body with a blank line
    # 2. Limit the subject line to 50 characters
    # 3. Capitalize the subject line
    # 4. Do not end the subject line with a period
    # 5. Use the imperative mood in the subject line
    # 6. Wrap the body at 72 characters
    # 7. Use the body to explain what and why vs. how
    #
    # How to Write a Git Commit Message http://chris.beams.io/posts/git-commit/
    
    # reference from http://memo.goodpatch.co/2016/07/beautiful-commits-with-emojis/
    # for https://github.com/bannzai/commit_template

--- GedatsuExample/GedatsuExample/ViewController.swift
+++ GedatsuExample/GedatsuExample/ViewController.swift
@@ -17,38 +17,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var customView: CustomView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-//        let subview = UIView()
-//        subview.translatesAutoresizingMaskIntoConstraints = false
-//        NSLayoutConstraint.activate([
-//            subview.widthAnchor.constraint(equalToConstant: 100),
-//            subview.widthAnchor.constraint(equalToConstant: 10),
-//        ])
-//        subview.setNeedsLayout()
-//        subview.layoutIfNeeded()
     }
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
-        printConstraint()
-    }
-    
-    func printConstraint() {
-//        print("view: ----")
-//        view.constraints.enumerated().forEach {
-//            print("\($0.0) --- ")
-//            _print(constraint: $0.1)
-//        }
-//        print("customView: ----")
-//        customView.constraints.enumerated().forEach {
-//            print("\($0): ---")
-//            _print(constraint: $1)
-//        }
-//        print("stackView: ----")
-//        stackView.constraints.enumerated().forEach {
-//            print("\($0.0) --- ")
-//            _print(constraint: $0.1)
-//        }
     }
 }
 
