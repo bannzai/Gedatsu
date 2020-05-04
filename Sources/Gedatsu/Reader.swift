34f709fd642e568b447c246d3770bd534e8a4836
bannzai <kingkong999yhirose@gmail.com>
2020-05-04 18:27:20 +0900

    :recycle: fix reader

--- Sources/Gedatsu/Reader.swift
+++ Sources/Gedatsu/Reader.swift
@@ -12,16 +12,19 @@ internal class ReaderImpl: Reader {
         pipe.fileHandleForReading.readInBackgroundAndNotify()
     }
     func read(closure: @escaping (String) -> Void) {
+        wait()
         NotificationCenter.default.addObserver(
             forName: FileHandle.readCompletionNotification,
             object: pipe.fileHandleForReading,
             queue: nil
         ) { [weak self] (notification) in
-                self?.wait()
-                if let data = notification.userInfo?[NSFileHandleNotificationDataItem] as? Data, let content = String(data: data, encoding: .utf8) {
-                    closure(content)
-                }
+            self?.wait()
+            if let data = notification.userInfo?[NSFileHandleNotificationDataItem] as? Data, let content = String(data: data, encoding: .utf8) {
+                closure(content)
+                return
+            } else {
+                fatalError("Unexpected data for userInfo: \(String(describing: notification.userInfo))")
+            }
         }
-        wait()
     }
 }
