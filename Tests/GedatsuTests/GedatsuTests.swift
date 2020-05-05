7ef968154746fa28e359a9157fe7a96a43fbafbc
bannzai <kingkong999yhirose@gmail.com>
2020-05-05 12:07:53 +0900

    :tada: Fix test

--- Tests/GedatsuTests/GedatsuTests.swift
+++ Tests/GedatsuTests/GedatsuTests.swift
@@ -1,13 +1,64 @@
+import UIKit
 import XCTest
-//@testable import Gedatsu
+@testable import Gedatsu
 
 final class GedatsuTests: XCTestCase {
-    
-    func testPipe() {
-//        let gedatsu = Gedatsu()
-//        gedatsu.input.fileHandleForReading.readabilityHandler = { [weak gedatsu] fileHandle in
-//            fileHandle.readInBackgroundAndNotify()
-//            String.init(data: fileHandle.availableData, encoding: .utf8)
-//        }
+    func testHookStdErrFlow() {
+        let reader = ReaderMock()
+        let writer = WriterMock()
+        let interceptor = InterceptorMock()
+        let gedatsu = Worker(reader: reader, writer: writer, interceptor: interceptor)
+        
+        prepare: do {
+            let data = "abc".data(using: .utf8)!
+            
+            let input = Pipe()
+            reader.readClosure = { data }
+            reader.underlyingReadingFileDescriptor = input.fileHandleForReading.fileDescriptor
+            reader.underlyingWritingFileDescriptor = input.fileHandleForWriting.fileDescriptor
+            
+            let output = Pipe()
+            writer.writeContentClosure = {
+                XCTAssertEqual($0, data)
+            }
+            writer.underlyingWritingFileDescriptor = output.fileHandleForWriting.fileDescriptor
+            
+            interceptor.canInterceptClosure = { true }
+        }
+        
+        UIView.swizzle()
+        gedatsu.open()
+        
+        before: do {
+            XCTAssertFalse(reader.readCalled)
+            XCTAssertFalse(writer.writeContentCalled)
+            XCTAssertFalse(interceptor.saveClosureCalled)
+            XCTAssertFalse(interceptor.canInterceptCalled)
+            XCTAssertFalse(interceptor.interceptCalled)
+        }
+
+        let view = UIView(frame: .init(origin: .zero, size: .init(width: 375, height: 667)))
+        view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            view.widthAnchor.constraint(equalToConstant: 100),
+            view.widthAnchor.constraint(equalToConstant: 10),
+        ])
+        
+        let expectation = XCTestExpectation(description: "Keep async queue")
+        interceptor.interceptClosure = {
+            expectation.fulfill()
+        }
+        
+        view.setNeedsLayout()
+        view.layoutIfNeeded()
+        
+        XCTWaiter().wait(for: [expectation], timeout: 10)
+        
+        after: do {
+            XCTAssertTrue(reader.readCalled)
+            XCTAssertFalse(interceptor.saveClosureCalled)
+            XCTAssertTrue(interceptor.canInterceptCalled)
+            XCTAssertTrue(interceptor.interceptCalled)
+        }
     }
 }
