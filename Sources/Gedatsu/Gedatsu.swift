0a9535f9a6f67138e163ffe27b9860a9d0fd2a91
bannzai <kingkong999yhirose@gmail.com>
2020-05-05 12:06:46 +0900

    :recycle: hold into Worker class

--- Sources/Gedatsu/Gedatsu.swift
+++ Sources/Gedatsu/Gedatsu.swift
@@ -1,7 +1,6 @@
 import Foundation
 import UIKit
 
-private var source: DispatchSourceRead!
 internal class Worker {
     private let lock = NSLock()
     internal let reader: Reader
@@ -13,6 +12,7 @@ internal class Worker {
         self.interceptor = interceptor
     }
     
+    private var source: DispatchSourceRead!
     internal func open() {
         _ = dup2(FileHandle.standardError.fileDescriptor, writer.writingFileDescriptor)
         _ = dup2(reader.writingFileDescriptor, STDERR_FILENO)
