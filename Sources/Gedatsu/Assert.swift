c6e8a8a0783b1bf1b5d32c7ec4517d7e45737349
bannzai <kingkong999yhirose@gmail.com>
2020-05-07 13:54:30 +0900

    :memo: early return when condition is false

--- Sources/Gedatsu/Assert.swift
+++ Sources/Gedatsu/Assert.swift
@@ -1,7 +1,10 @@
 import Foundation
 
 func gedatsuAssert(_ condition: Bool, _ message: String = "", file: StaticString = #file, line: UInt = #line) {
-    print("[GEDATSU WARNING]: Unexpected behavior about \(message) with condition \(condition) in \(file):\(line)")
+    if condition {
+        return
+    }
+    print("[GEDATSU WARNING]: Unexpected behavior about \(message) in \(file):\(line)")
     #if DEBUG_GEDATSU
     Swift.assert(condition, message)
     #endif
