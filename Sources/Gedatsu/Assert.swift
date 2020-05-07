23badc9670e531b21445c577512e8abd62c7ed20
bannzai <kingkong999yhirose@gmail.com>
2020-05-07 15:02:31 +0900

    :memo: add flag for ignore warn log

--- Sources/Gedatsu/Assert.swift
+++ Sources/Gedatsu/Assert.swift
@@ -1,11 +1,16 @@
 import Foundation
 
-func gedatsuAssert(_ condition: Bool, _ message: String = "", file: StaticString = #file, line: UInt = #line) {
+func gedatsuAssert(_ condition: Bool, _ message: String = "", ignoreWarnLog: Bool = false, file: StaticString = #file, line: UInt = #line) {
     if condition {
         return
     }
-    print("[GEDATSU WARNING]: Unexpected behavior about \(message) in \(file):\(line)")
+    if !ignoreWarnLog {
+        print("[GEDATSU WARNING]: Unexpected behavior about \(message) in \(file):\(line)")
+    }
     #if DEBUG_GEDATSU
+    if ignoreWarnLog {
+        print("[GEDATSU WARNING]: Unexpected behavior about \(message) in \(file):\(line)")
+    }
     Swift.assert(condition, message)
     #endif
 }
