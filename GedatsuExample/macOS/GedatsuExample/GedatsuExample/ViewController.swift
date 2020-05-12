e4a4b85d1cdae6a1307cdf19307938c19631d0e3
bannzai <kingkong999yhirose@gmail.com>
2020-05-13 08:54:25 +0900

    :sparkles: add GedatsuExample for macOS

---
+++ GedatsuExample/macOS/GedatsuExample/GedatsuExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GedatsuExample
+//
+//  Created by yudai-hirose on 2020/05/13.
+//  Copyright © 2020 bannzai. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
