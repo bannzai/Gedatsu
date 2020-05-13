a536be6d77164a210efbe51c534473ba3301458c
bannzai <kingkong999yhirose@gmail.com>
2020-05-13 09:12:19 +0900

    :sparkles: add alias for Window

--- Sources/Gedatsu/Alias.swift
+++ Sources/Gedatsu/Alias.swift
@@ -2,10 +2,12 @@ import Foundation
 
 #if os(iOS)
 import UIKit
-public typealias ViewType = UIView
+public typealias WindowType = UIWindow
+public typealias ViewType = ViewType
 public typealias ResponderType = UIResponder
 #elseif os(macOS)
 import AppKit
+public typealias WindowType = NSWindow
 public typealias ViewType = NSView
 public typealias ResponderType = NSResponder
 #endif
