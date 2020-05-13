5178d5f78427b5ef37488670324b57f6aefd6467
bannzai <kingkong999yhirose@gmail.com>
2020-05-13 19:01:23 +0900

    :bug: missing specify identifier

--- Sources/Gedatsu/FormatterHelper.swift
+++ Sources/Gedatsu/FormatterHelper.swift
@@ -7,22 +7,25 @@ func space(level: Int) -> String {
     }
     return indent
 }
-func debugContent(node: Node) -> String {
-    if let identifier = (node.responder as? ViewType)?._accessibilityIdentifier {
-        return "\(identifier):\(addres(of: node.responder))"
+func responderIdentifier(node: Node) -> String {
+    if let identifier = (node.responder as? ViewType)?._accessibilityIdentifier, !identifier.isEmpty {
+        return "\(identifier)"
     }
     #if os(iOS)
     // FIXME: How to access NSAccessibility.accessibilityLabel then macOS
-    if let label = node.responder.accessibilityLabel {
-        return "\(label): \(addres(of: node.responder))"
+    if let label = node.responder.accessibilityLabel, !label.isEmpty {
+        return "\(label)"
     }
     #endif
+    return "\(type(of: node.responder))"
+}
+func debugContent(node: Node) -> String {
     switch node.hasAmbiguous {
     case true:
         let attributes = node.attributes.map { "\($0.displayName)" }.joined(separator: ", ")
-        return "❌ \(type(of: node.responder)): \(addres(of: node.responder)), ambiguous attributes: \(attributes)"
+        return "❌ \(responderIdentifier(node: node)): \(addres(of: node.responder)), ambiguous attributes: \(attributes)"
     case false:
-        return "\(type(of: node.responder)): \(addres(of: node.responder))"
+        return "\(responderIdentifier(node: node)): \(addres(of: node.responder))"
     }
 }
 func itemType(of item: AnyObject) -> String {
