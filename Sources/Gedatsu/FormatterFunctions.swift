aac8d7973d0868eb1adfb466885cc59f6eb8e3e1
bannzai <kingkong999yhirose@gmail.com>
2020-06-06 16:51:52 +0900

    :sparkles: header message with invalid pattern

--- Sources/Gedatsu/FormatterFunctions.swift
+++ Sources/Gedatsu/FormatterFunctions.swift
@@ -1,4 +1,9 @@
 import Foundation
+#if os(iOS)
+import UIKit
+#elseif os(macOS)
+import AppKit
+#endif
 
 public func buildTreeContent(context: Context) -> String {
     var content = ""
@@ -12,6 +17,7 @@ public func buildTreeContent(context: Context) -> String {
     }
     return content
 }
+
 public func buildHeader(context: Context) -> String {
     return context.exclusiveConstraints.compactMap { constraint in
         let address = addres(of: constraint)
@@ -19,13 +25,32 @@ public func buildHeader(context: Context) -> String {
         case (.none, .none):
             return "NSLayoutConstraint: \(address) Unknown case. \(constraint.displayIdentifier)"
         case (.some(let lhs), .some(let rhs)):
-            let (lAttribute, rAttribute) = (constraint.firstAttribute.displayName, constraint.secondAttribute.displayName)
+            let (lAttribute, rAttribute) = (constraint.firstAttribute, constraint.secondAttribute)
             let relation = constraint.relation
-            return "NSLayoutConstraint: \(address) \(itemType(of: lhs)).\(lAttribute) \(relation.displayName) \(itemType(of: rhs)).\(rAttribute) \(constraint.displayIdentifier)"
+            return buildHeaderContent(
+                displayables: [lAttribute, rAttribute, relation],
+                defaultContent: "NSLayoutConstraint: \(address) \(itemType(of: lhs)).\(lAttribute.displayName) \(relation.displayName) \(itemType(of: rhs)).\(rAttribute.displayName) \(constraint.displayIdentifier)"
+            )
         case (.some(let item), .none):
-            return "NSLayoutConstraint: \(address) \(itemType(of: item)).\(constraint.firstAttribute.displayName) \(constraint.relation.displayName) \(constraint.constant) \(constraint.displayIdentifier)"
+            return buildHeaderContent(displayables: [constraint.firstAttribute, constraint.relation], defaultContent: "NSLayoutConstraint: \(address) \(itemType(of: item)).\(constraint.firstAttribute.displayName) \(constraint.relation.displayName) \(constraint.constant) \(constraint.displayIdentifier)")
         case (.none, .some(let item)):
-            return "NSLayoutConstraint: \(address) \(itemType(of: item)).\(constraint.secondAttribute.displayName) \(constraint.relation.displayName) \(constraint.constant) \(constraint.displayIdentifier)"
+            return buildHeaderContent(displayables: [constraint.secondAttribute, constraint.relation], defaultContent: "NSLayoutConstraint: \(address) \(itemType(of: item)).\(constraint.secondAttribute.displayName) \(constraint.relation.displayName) \(constraint.constant) \(constraint.displayIdentifier)")
         }
     }.joined(separator: "\n")
+}
+
+private func buildHeaderContent(displayables: [HasDisplayName], defaultContent content: String) -> String {
+    let errorMessage = """
+Gedatsu catch unknown attribute or relation pattern. See issues for a solution to this problem. If that doesn't work, please create a new issue."
+https://github.com/bannzai/gedatsu/issues
+"""
+    switch displayables.allSatisfy(\.isValid) {
+    case true:
+        return content
+    case false:
+        return """
+        \(errorMessage)
+        \(content)
+        """
+    }
 }
