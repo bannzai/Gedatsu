48ba8118cc11234616544e3146ed83da02818a28
bannzai <kingkong999yhirose@gmail.com>
2020-06-06 17:00:58 +0900

    :recycle: fix build header with error message

--- Sources/Gedatsu/FormatterFunctions.swift
+++ Sources/Gedatsu/FormatterFunctions.swift
@@ -12,20 +12,26 @@ public func buildTreeContent(context: Context) -> String {
     }
     return content
 }
-public func buildHeader(context: Context) -> String {
-    return context.exclusiveConstraints.compactMap { constraint in
+public func buildHeader(context: Context) -> (String, Bool) {
+    var shouldShowErrorMessage = false
+    let validate: ([HasDisplayName]) -> Bool = { $0.allSatisfy(\.isValid) }
+    let content = context.exclusiveConstraints.compactMap { constraint in
         let address = addres(of: constraint)
         switch (constraint.firstItem, constraint.secondItem) {
         case (.none, .none):
             return "NSLayoutConstraint: \(address) Unknown case. \(constraint.displayIdentifier)"
         case (.some(let lhs), .some(let rhs)):
-            let (lAttribute, rAttribute) = (constraint.firstAttribute.displayName, constraint.secondAttribute.displayName)
+            let (lAttribute, rAttribute) = (constraint.firstAttribute, constraint.secondAttribute)
             let relation = constraint.relation
-            return "NSLayoutConstraint: \(address) \(itemType(of: lhs)).\(lAttribute) \(relation.displayName) \(itemType(of: rhs)).\(rAttribute) \(constraint.displayIdentifier)"
+            shouldShowErrorMessage = shouldShowErrorMessage || validate([lAttribute, rAttribute, relation])
+            return "NSLayoutConstraint: \(address) \(itemType(of: lhs)).\(lAttribute.displayName) \(relation.displayName) \(itemType(of: rhs)).\(rAttribute.displayName) \(constraint.displayIdentifier)"
         case (.some(let item), .none):
+            shouldShowErrorMessage = shouldShowErrorMessage || validate([constraint.firstAttribute, constraint.relation])
             return "NSLayoutConstraint: \(address) \(itemType(of: item)).\(constraint.firstAttribute.displayName) \(constraint.relation.displayName) \(constraint.constant) \(constraint.displayIdentifier)"
         case (.none, .some(let item)):
+            shouldShowErrorMessage = shouldShowErrorMessage || validate([constraint.secondAttribute, constraint.relation])
             return "NSLayoutConstraint: \(address) \(itemType(of: item)).\(constraint.secondAttribute.displayName) \(constraint.relation.displayName) \(constraint.constant) \(constraint.displayIdentifier)"
         }
     }.joined(separator: "\n")
+    return (content, shouldShowErrorMessage)
 }
